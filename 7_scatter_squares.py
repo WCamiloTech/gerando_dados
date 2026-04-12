c761e3ff5b1d68ff1b3eea2eb4b552a64f230881
Williames Camilo <williamescw@gmail.com>
2026-04-11 22:52:08 -0300

    Salvando gráficos automaticamente

---
+++ 7_scatter_squares.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+
+#Salvando seus gráficos automaticamente
+
+x_values = list(range(1001))
+y_values = [x**2 for x in x_values]
+
+plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolors='none', s=40)
+
+# Define o título do gráfico e nomeia os eixos
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+#Define o intervalo para cada eixo
+plt.axis([0, 1100, 0, 1100000])
+
+# Define o tamanho dos rótulos das marcações
+plt.tick_params(axis="both", which="major", labelsize=14)
+
+#Desativando notação cientifica no eixo y
+plt.ticklabel_format(style="plain", axis="y")
+
+#Salvando o gráfico
+plt.savefig("squares_plot.png", bbox_inches="tight")
