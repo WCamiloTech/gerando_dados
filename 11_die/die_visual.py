3c32ad936a7d1bf17f8ed96bab1617923048b42a
Williames Camilo <williamescw@gmail.com>
2026-04-14 12:53:43 -0300

    Lançando o dado

---
+++ 11_die/die_visual.py
@@ -0,0 +1,13 @@
+from die import Die
+
+# Cria um D6
+die = Die()
+
+# Faz alguns lançamentos e armazena os resultados em uma lista
+results = []
+
+for roll_num in range(100):
+    result = die.roll()
+    results.append(result)
+
+print(results)
