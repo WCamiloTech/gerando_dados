a9f2ad146dcae2958876824b24d016f5f9dc7eab
Williames Camilo <williamescw@gmail.com>
2026-04-17 12:40:41 -0300

    Datetime plotandoa as datas

---
+++ arquivos_csv/01/highs_lows_6.py
@@ -0,0 +1,43 @@
+import csv
+from matplotlib import pyplot as plt
+from datetime import datetime
+# Extraindo e lendo os dados
+
+# Módulo datetime 
+# Plotando as datas
+
+# Obtém as datas e as temperaturas máximas do arquivo
+filename = 'sitka_weather_07-2014.csv'
+
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    highs = []
+    dates = []
+
+    for row in reader:
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
+        high = int(row[1])
+        highs.append(high)
+
+    print(highs)
+    
+    # Faz a plotagem dos dados
+    fig = plt.figure(dpi=128, figsize=(10, 6))
+    plt.plot(dates, highs, c='red')
+    
+    # Formata o gráfico
+    plt.title('Daily high temperatures, July 2014', fontsize=24)
+    plt.xlabel('', fontsize=16)
+    fig.autofmt_xdate()
+    plt.ylabel('Temperature (F)', fontsize=16)
+    plt.tick_params(axis='y', which='major', labelsize=16)
+    
+
+    plt.show()
+
+    
+            
+        
+    
