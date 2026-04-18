eb63abb69a949007dd3da2bf89de420f8d6b5bdc
Williames Camilo <williamescw@gmail.com>
2026-04-18 12:46:31 -0300

    Sombreando uma área do gráfico

---
+++ arquivos_csv/01/highs_lows_9.py
@@ -0,0 +1,42 @@
+import csv
+from matplotlib import pyplot as plt
+from datetime import datetime
+
+# Sombreando uma área do gráfico
+
+# Extraindo e lendo os dados
+# Obtém as datas e as temperaturas máximas do arquivo
+filename = 'sitka_weather_2014.csv'
+
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    highs = []
+    lows = []
+    dates = []
+    
+
+    for row in reader:
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
+        high = int(row[1])
+        highs.append(high)
+        low = int(row[3])
+        lows.append(low)
+        
+
+    
+    # Faz a plotagem dos dados
+    fig = plt.figure(dpi=128, figsize=(10, 6))
+    plt.plot(dates, highs, c='red', alpha=0.5)
+    plt.plot(dates, lows, c='blue', alpha=0.5)
+    plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+    
+    # Formata o gráfico
+    plt.title('Daily high temperatures - 2014', fontsize=24)
+    plt.xlabel('', fontsize=16)
+    fig.autofmt_xdate()
+    plt.ylabel('Temperature (F)', fontsize=16)
+    plt.tick_params(axis='y', which='major', labelsize=16)
+    
+    plt.show()
