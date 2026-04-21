a27f3b7e6aad2567743b5e7dc5929be84297b976
Williames Camilo <williamescw@gmail.com>
2026-04-21 16:11:41 -0300

    Adicionando links que podem ser clicados em nosso gráfico

---
+++ 18_trabalhando_com_APIs/python_repos_7.py
@@ -0,0 +1,60 @@
+import pygal
+import requests
+from pygal.style import LightColorizedStyle as LCS
+from pygal.style import RotateStyle as RS
+from pygal.style import LightenStyle as LS
+
+# Adicionando links que podem ser clicados em nosso gráfico
+# Faz uma chamada de API e armazena a resposta
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print(f'Status code: {r.status_code}')
+
+# Armazena a resposta da API em uma variável
+response_dict = r.json()
+
+# Processa o resultado
+print(response_dict.keys())
+
+# Armazena a respota da API em um variável
+print(f"Total repositórios: {response_dict['total_count']}")
+
+# Explora informações sobre os repositórios
+repo_dicts = response_dict['items']
+print(f"Number of items: {len(repo_dicts)}")
+names = []
+plot_dicts = []
+
+print("\nSelected information about each repository:")
+
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    
+    plot_dict = {
+        'value': repo_dict['stargazers_count'], 
+        'label': repo_dict['description'],
+        'xlink': repo_dict['html_url'], 
+        }
+    
+    plot_dicts.append(plot_dict)
+    
+    # Cria a visualização
+    my_style = LS('#333366', base_style=LCS)
+    my_config = pygal.Config()
+    my_config.x_label_rotation = 45
+    my_config.show_legend = False
+    my_config.title_font_size = 24
+    my_config.label_font_size = 14
+    my_config.major_label_font_size = 18
+    my_config.truncate_label = 15
+    my_config.show_y_guides = False
+    my_config.width = 1000
+    chart = pygal.Bar(my_config, style=my_style)
+    chart.title = 'Most-Starred Python Projects on GitHub'
+    chart.x_labels = names
+    chart.add('', plot_dicts)
+    chart.render_to_file('python_repos_4.svg')
+
+
+
+
