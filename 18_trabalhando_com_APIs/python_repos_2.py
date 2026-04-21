94881463b6ffe02a46097851c1f3724529dee98b
Williames Camilo <williamescw@gmail.com>
2026-04-20 21:48:26 -0300

    Trabalhando com o dicionário de resposta

---
+++ 18_trabalhando_com_APIs/python_repos_2.py
@@ -0,0 +1,37 @@
+import requests
+
+# Trabalhando com o dicionário de resposta
+
+# Faz uma chamada de API e armazena a resposta
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print(f'Status code: {r.status_code}')
+
+# Armazena a resposta da API em uma variável
+response_dict = r.json()
+
+# Processa o resultado
+print(response_dict.keys())
+
+# Armazena a respota da API em um variável
+print(f"Total repositórios: {response_dict['total_count']}")
+
+# Explora informações sobre os repositórios
+repo_dicts = response_dict['items']
+print(f'Repositories returned: {len(repo_dicts)}')
+
+# Analisando o primeiro repositório
+repo_dict = repo_dicts[0]
+
+print(f'\nKeys: {len(repo_dict)}')
+print(f'\nSelected information abou first repository: ')
+print(f"Name: {repo_dict['name']}")
+print(f"Owner: {repo_dict['owner']['login']}")
+print(f"Starts: {repo_dict['stargazers_count']}")
+print(f"Repository:, {repo_dict['html_url']}")
+print(f"Created:, {repo_dict['created_at']}")
+print(f"Updated:, {repo_dict['updated_at']}") 
+print(f"Description:, {repo_dict['description']}")
+
+# for key in sorted(repo_dict.keys()):
+#     print(key)
