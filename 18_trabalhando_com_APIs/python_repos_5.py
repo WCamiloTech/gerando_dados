19f061669d3161fe35ae17a4b24427f2fc2dafb1
Williames Camilo <williamescw@gmail.com>
2026-04-21 13:07:53 -0300

    Aperfeiçoando os gráficos do Pygal

--- 18_trabalhando_com_APIs/python_repos_5.py
+++ 18_trabalhando_com_APIs/python_repos_5.py
@@ -4,7 +4,7 @@ from pygal.style import LightColorizedStyle as LCS
 from pygal.style import RotateStyle as RS
 from pygal.style import LightenStyle as LS
 
-# Visualizando os repositórios usando o Pygal
+# Aperfeiçoando os gráficos do Pygal
 
 # Faz uma chamada de API e armazena a resposta
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -33,17 +33,21 @@ for repo_dict in repo_dicts:
     
     # Cria a visualização
     my_style = LS('#333366', base_style=LCS)
-    chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+    my_config = pygal.Config()
+    my_config.x_label_rotation = 45
+    my_config.show_legend = False
+    my_config.title_font_size = 24
+    my_config.label_font_size = 14
+    my_config.major_label_font_size = 18
+    my_config.truncate_label = 15
+    my_config.show_y_guides = False
+    my_config.width = 1000
+    chart = pygal.Bar(my_config, style=my_style)
     chart.title = 'Most-Starred Python Projects on GitHub'
     chart.x_labels = names
     chart.add('', stars)
-    chart.render_to_file('python_repos_1.svg')
-    
-    # print('\nName:', repo_dict['name'])
-    # # print('Owner:', repo_dict['owner']['login']) 
-    # print('Stars:', repo_dict['stargazers_count'])
-    # print('Repository:', repo_dict['html_url']) 
-    # # print('Description:', repo_dict['description']) 
+    chart.render_to_file('python_repos_2.svg')
+
 
 
 
